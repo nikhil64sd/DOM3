3809f61555feb56d335ec5784b5a8f36eb534a0e
nikhil64sd <nikhilgoli454@gmail.com>
2023-08-27 17:50:31 +0530

    dayone

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelector('h1').addEventListener("click",function(){
+    document.querySelector('h1').textContent = document.querySelector('h1').textContent.toUpperCase()
+
+})
+
+
+
